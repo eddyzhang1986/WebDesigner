1c7ef3662d2c9862fe8ee0c002bdc1b372c7c07c
eddyzhang1986 <eddyzhang1986@msn.cn>
2018-06-26 07:55:53 +0800

    增加一个图片预览

--- src/device/Device.tsx
+++ src/device/Device.tsx
@@ -3,6 +3,14 @@ import { IPhone } from "./iphone/iphone";
 
 export class Device extends React.Component {
   render() {
-    return <IPhone>test111</IPhone>;
+    return (
+      <IPhone>
+        <img
+          width={320}
+          height={562}
+          src={require("../../resources/homepage.jpg")}
+        />
+      </IPhone>
+    );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
         test: /\.css$/,
         use: ["style-loader", "css-loader"]
       },
