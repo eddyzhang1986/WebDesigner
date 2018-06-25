307c95f41906986560719bc0e0188e66c4e041ec
eddyzhang1986 <eddyzhang1986@msn.cn>
2018-06-25 17:00:56 +0800

    json format

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,19 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Device } from "./device/device";
+import { Editor } from "./components/editor";
 import "./index.css";
+const Json = require("./data/schema.json");
 
-ReactDOM.render(<Device />, document.getElementById("container"));
+ReactDOM.render(
+  <div>
+    {" "}
+    <div>
+      <Device />
+    </div>{" "}
+    <div>
+      <Editor onChange={() => {}} value={JSON.stringify(Json, null, 2)} />
+    </div>
+  </div>,
+  document.getElementById("container")
+);
