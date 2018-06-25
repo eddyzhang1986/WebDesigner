cacdb55f05f267c4abeacdd853f268faa500a763
eddyzhang1986 <eddyzhang1986@msn.cn>
2018-06-25 13:06:22 +0800

    hot loader 设置还原

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,8 @@
-const webpack = require("webpack");
+//const webpack = require("webpack");
 
 module.exports = {
-  entry: [
-    "webpack-dev-server/client?http://localhost:9000/",
-    "./src/index.tsx"
-  ],
+  entry: "./src/index.tsx", //"webpack-dev-server/client?http://localhost:9000/",
+
   output: {
     filename: "bundle.js",
     path: __dirname + "/dist"
@@ -36,7 +34,7 @@ module.exports = {
     ]
   },
 
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+  //plugins: [new webpack.HotModuleReplacementPlugin()],
   // When importing a module whose path matches one of the following, just
   // assume a corresponding global variable exists and use that instead.
   // This is important because it allows us to avoid bundling all of our
