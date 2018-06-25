7028b8ee8d62f75691f580f0809909b923cb5458
eddyzhang1986 <eddyzhang1986@msn.cn>
2018-06-25 12:49:24 +0800

    hot loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const webpack = require("webpack");
+
 module.exports = {
   entry: "./src/index.tsx",
   output: {
@@ -31,6 +33,7 @@ module.exports = {
     ]
   },
 
+  plugins: [new webpack.HotModuleReplacementPlugin()],
   // When importing a module whose path matches one of the following, just
   // assume a corresponding global variable exists and use that instead.
   // This is important because it allows us to avoid bundling all of our
