df64adbdca3495c6ac6fb71e196861cfce0c2364
eddyzhang1986 <eddyzhang1986@msn.cn>
2018-06-25 17:16:33 +0800

    json format

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,25 @@ import * as ReactDOM from "react-dom";
 import { Device } from "./device/device";
 import { Editor } from "./components/editor";
 import "./index.css";
+const ReactRouter = require("react-router-dom");
+const { HashRouter: Router, Route, Link } = ReactRouter;
+
 const Json = require("./data/schema.json");
 
+const Home = () => {
+  return <Device>test</Device>;
+};
+
+const Format = () => {
+  return <Editor onChange={() => {}} value={JSON.stringify(Json, null, 2)} />;
+};
+
 ReactDOM.render(
-  <div>
-    {" "}
+  <Router>
     <div>
-      <Device />
-    </div>{" "}
-    <div>
-      <Editor onChange={() => {}} value={JSON.stringify(Json, null, 2)} />
+      <Route exact path="/" component={Home} />
+      <Route path="/format" component={Format} />
     </div>
-  </div>,
+  </Router>,
   document.getElementById("container")
 );
