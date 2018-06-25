fb399ba36ecf4d7092b1b17aed0d5370e43180e3
刘宾 <915249126@qq.com>
2018-06-25 18:36:55 +0800

    add packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
-//const webpack = require("webpack");
+const webpack = require('webpack')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.tsx", //"webpack-dev-server/client?http://localhost:9000/",
@@ -37,7 +39,17 @@ module.exports = {
       { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
     ]
   },
-
+  plugins:[
+    new HtmlWebpackPlugin({
+        title:'react 学习',
+        inject:'body',
+        filename:'index.html',
+        template:path.resolve(__dirname, "index.html")
+    }),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //     names: ["vandor", "manifest"]
+    // })
+   ],
   //plugins: [new webpack.HotModuleReplacementPlugin()],
   // When importing a module whose path matches one of the following, just
   // assume a corresponding global variable exists and use that instead.
