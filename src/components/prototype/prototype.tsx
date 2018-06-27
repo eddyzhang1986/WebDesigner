9690a9d7ff0767a45661d415563bace8a6e9121d
eddyzhang1986 <eddyzhang1986@msn.cn>
2018-06-28 00:05:08 +0800

    手绘原型图

--- src/components/prototype/prototype.tsx
+++ src/components/prototype/prototype.tsx
@@ -1,12 +1,68 @@
 import * as React from "react";
-import { Row } from "react-bootstrap";
+import { Grid, Row, Col } from "react-bootstrap";
 
 export class Prototype extends React.Component {
   render() {
     return (
-      <Row>
-        <div style={{ color: "white" }}>Prototype</div>
-      </Row>
+      <Grid style={{ width: "320px" }}>
+        <Row
+          style={{
+            backgroundImage: "url(../../../resources/images/bannerbg.jpg)",
+            height: "386px",
+            backgroundSize: "100% 100%",
+            MozBackgroundSize: "100% 100%"
+          }}
+        >
+          <Row style={{ height: "40px" }}>
+            <Col md={12} xs={12} sm={12}>
+              header
+            </Col>
+          </Row>
+          <Row
+            style={{
+              backgroundImage: "url(../../../resources/images/swiper.png)",
+              height: "206px",
+              backgroundSize: "100% 100%",
+              MozBackgroundSize: "100% 100%",
+              margin: "0px auto"
+            }}
+          />
+          <Row>
+            <Col md={1} xs={1} sm={1} />
+            <Col md={2} xs={2} sm={2}>
+              <img
+                src={require("../../../resources/images/ball1.png")}
+                width={40}
+              />
+            </Col>
+            <Col md={2} xs={2} sm={2} style={{ textAlign: "center" }}>
+              <img
+                src={require("../../../resources/images/ball2.png")}
+                width={40}
+              />
+            </Col>
+            <Col md={2} xs={2} sm={2} style={{ textAlign: "center" }}>
+              <img
+                src={require("../../../resources/images/ball3.png")}
+                width={40}
+              />
+            </Col>
+            <Col md={2} xs={2} sm={2} style={{ textAlign: "center" }}>
+              <img
+                src={require("../../../resources/images/ball4.png")}
+                width={40}
+              />
+            </Col>
+            <Col md={2} xs={2} sm={2} style={{ textAlign: "center" }}>
+              <img
+                src={require("../../../resources/images/ball5.png")}
+                width={40}
+              />
+            </Col>
+            <Col md={1} xs={1} sm={1} />
+          </Row>
+        </Row>
+      </Grid>
     );
   }
 }
