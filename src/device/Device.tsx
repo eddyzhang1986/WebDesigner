576406715299f8841088409cb4deae81bee4add1
eddyzhang1986 <eddyzhang1986@msn.cn>
2018-06-28 12:53:20 +0800

    add react json schema

--- src/device/Device.tsx
+++ src/device/Device.tsx
@@ -1,12 +1,25 @@
 import * as React from "react";
 import { IPhone } from "./iphone/iphone";
+import Form from "react-jsonschema-form";
+import { Grid, Row, Col } from "react-bootstrap";
+
+const schema = require("../data/form.json");
 
 export class Device extends React.Component {
   render() {
     return (
-      <IPhone>
-        <img width={320} src={require("../../resources/homepage.jpg")} />
-      </IPhone>
+      <Grid>
+        <Row>
+          <Col md={6} xs={6} sm={6}>
+            <IPhone>
+              <img width={320} src={require("../../resources/homepage.jpg")} />
+            </IPhone>
+          </Col>
+          <Col md={6} xs={6} sm={6}>
+            <Form schema={schema} />
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }
