bafae86829d470befd8522b677d3d2d4bcf7a219
eddyzhang1986 <eddyzhang1986@msn.cn>
2018-06-28 13:21:05 +0800

    文本框编辑

--- src/device/Device.tsx
+++ src/device/Device.tsx
@@ -2,21 +2,28 @@ import * as React from "react";
 import { IPhone } from "./iphone/iphone";
 import Form from "react-jsonschema-form";
 import { Grid, Row, Col } from "react-bootstrap";
+import { Editor } from "../components/editor";
 
 const schema = require("../data/form.json");
+const home = require("../data/home.json");
 
 export class Device extends React.Component {
   render() {
     return (
       <Grid>
         <Row>
-          <Col md={6} xs={6} sm={6}>
+          <Col md={4} xs={4} sm={4}>
             <IPhone>
               <img width={320} src={require("../../resources/homepage.jpg")} />
             </IPhone>
           </Col>
-          <Col md={6} xs={6} sm={6}>
+          <Col md={8} xs={8} sm={8}>
             <Form schema={schema} />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} xs={12} sm={12}>
+            <Editor onChange={() => {}} value={JSON.stringify(home, null, 2)} />;
           </Col>
         </Row>
       </Grid>
